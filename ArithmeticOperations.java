74bde51a8b009d0e4f7b4edae9f33f60d5b8324d
Anshifa2005 <anshifatp001@gmail.com>
2025-11-14 14:57:31 +0530

    Add files via upload

---
+++ ArithmeticOperations.java
@@ -0,0 +1,35 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+
+       
+        if (args.length < 2) {
+            System.out.println("Please provide two numbers as command line arguments.");
+            return;
+        }
+
+        
+        double num1 = Double.parseDouble(args[0]);
+        double num2 = Double.parseDouble(args[1]);
+
+        
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+
+        
+        if (num2 != 0) {
+            double quotient = num1 / num2;
+            double remainder = num1 % num2;
+
+            System.out.println("Quotient: " + quotient);
+            System.out.println("Remainder: " + remainder);
+        } else {
+            System.out.println("Quotient: Undefined (division by zero)");
+            System.out.println("Remainder: Undefined (division by zero)");
+        }
+    }
+}
